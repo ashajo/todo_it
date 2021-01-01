3385b27a78f67912f7256d5373271a61221bcda5
asha joseph <ashajo183@gmail.com>
2021-01-01 21:41:21 +0100

    junittest

---
+++ junittest/se/lexicon/asha/model/PersonTest.java
@@ -0,0 +1,28 @@
+package se.lexicon.asha.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void getPersonId() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+}
